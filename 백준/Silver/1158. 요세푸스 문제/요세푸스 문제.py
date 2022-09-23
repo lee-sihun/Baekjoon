e4d9c1ce2a1d28bb2dead52520165fb663e54b36
lee-sihun <111095268+lee-sihun@users.noreply.github.com>
2022-09-23 09:21:53 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+n = list(range(1, n+1))
+cnt = k-1
+result = []
+
+for i in range(len(n)):
+    # if cnt > len(n):
+    #     cnt = cnt - len(n)
+    # if cnt == len(n):
+    #     cnt = 0
+    while cnt >= len(n):
+        cnt = cnt - len(n)
+        if cnt == len(n):
+            cnt = 0
+            # print(result,cnt,len(n),n)
+            break
+        # print(result,cnt,len(n),n)
+
+    result.append(n[cnt])
+    del n[cnt]
+    cnt += k-1
+    # print(result,cnt,len(n),n)
+
+print('<'+str(result)[1:-1]+'>')
+
+# cnt 2 / 3
+# 1 2 3 4 5 6 7
+
+# cnt 4 / 3 6
+# 1 2 4 5 6 7
+
+# cnt 6 / 3 6
+# 1 2 4 5 7
