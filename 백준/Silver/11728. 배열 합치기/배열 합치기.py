a99377e8bb7434ac51222bd52102e684371f4740
lee-sihun <111095268+lee-sihun@users.noreply.github.com>
2022-09-27 08:23:54 +0900

    [Silver V] Title: 배열 합치기, Time: 1252 ms, Memory: 343280 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = sorted(a + b)
+
+for i in c:
+    print(i,end=' ')
